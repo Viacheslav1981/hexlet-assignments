48862f08d2ae5e519fdc3c252844007ad28161c2
@hexlet/cli <support@hexlet.io>
2023-01-26 14:21:04 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -5,27 +5,8 @@ import java.util.Arrays;
 
 // BEGIN
 class App {
-
-    private static String data1 = " [program:options]\n" +
-            "environment=\"X_FORWARDED_variable=value,  \"\n" +
-            "\n" +
-            "environment=\"key5=value5,X_FORWARDED_var3=value,key6=value6\"" +
-            "\n" +
-            "environment=\"X_FORWARDED_mail=tirion@google.com,X_FORWARDED_HOME=/home/tirion,language=en\"" +
-            "\n" +
-            "environment=\"key5=value5,X_FORWARDED_var3=value,key6=value6\"" +
-            "\n" +
-            "environment=\"pwd=/temp,user=tirion\"" +
-            "\n" +
-            "key=value" +
-            "\n" +
-            "command=X_FORWARDED_HOME=/ cd ~";
-
-
     public static void main(String[] args) {
-
-        System.out.println(getForwardedVariables(data1));
-
+        
     }
 
     public static String getForwardedVariables(String data) {
