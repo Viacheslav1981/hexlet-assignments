8027fc29e5ab38ba0f180ffa28b9f6d1fc52b16b
@hexlet/cli <support@hexlet.io>
2023-09-20 09:20:22 +0300

    submit java-web-ru/post-form

---
+++ java-web-ru/post-form/src/main/java/exercise/App.java
@@ -0,0 +1,68 @@
+package exercise;
+
+import io.javalin.Javalin;
+
+import java.util.List;
+import java.util.Collections;
+
+import exercise.model.User;
+import exercise.dto.users.UsersPage;
+import exercise.repository.UserRepository;
+import org.apache.commons.lang3.StringUtils;
+import exercise.util.Security;
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        app.get("/", ctx -> {
+            ctx.render("index.jte");
+        });
+
+        app.get("/users", ctx -> {
+            List<User> users = UserRepository.getEntities();
+            var page = new UsersPage(users);
+            ctx.render("users/index.jte", Collections.singletonMap("page", page));
+        });
+
+        app.get("/users/build", ctx -> {
+                    ctx.render("users/build.jte");
+                }
+        );
+
+        app.post("/users", ctx -> {
+            var firstName = ctx.formParam("firstName").trim();
+            var lastName = ctx.formParam("lastName").trim();
+            var email = ctx.formParam("email").toString().trim().toLowerCase();
+            var password = ctx.formParam("password").trim();
+            var passwordConfirmation = ctx.formParam("passwordConfirmation");
+
+           // String cap = str.substring(0, 1).toUpperCase() + str.substring(1);
+             firstName = firstName.substring(0, 1).toUpperCase() + firstName.substring(1);
+            lastName = lastName.substring(0, 1).toUpperCase() + lastName.substring(1);
+
+
+            //  var course = new User(name, email, password);
+         //   UserRepository.save(course);
+          //  if (!(password.equals(passwordConfirmation))) {
+           //     throw new Exception("No password");
+          //  }
+            password = Security.encrypt(password);
+
+            var user = new User(firstName, lastName, email, password);
+            UserRepository.save(user);
+            ctx.redirect("/users");
+        });
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
